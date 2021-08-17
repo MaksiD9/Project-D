1255f64c20736d47337eb36ee9fdc664fc739640
Max <maximka5991@gmail.com>
2021-08-17 13:56:37 +0300

    download

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
 import Form from './modules/form';
 import ShowInfo from './modules/showInfo';
+import Download from './modules/download';
 
 
 
@@ -54,5 +55,7 @@ window.addEventListener('DOMContentLoaded', () => {
     new Form('.form').init();
 
     new ShowInfo('.plus__content').init();
+
+    new Download('.download').init();
  
 });
